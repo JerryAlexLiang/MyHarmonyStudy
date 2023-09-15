7bcea52e3cb6f6dcf7a359a65e00181b4f11bf73
JerryAlexLiang <1935990239@qq.com>
2023-09-15 17:51:38 +0800

    应用程序框架学习

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -4,18 +4,25 @@ import window from '@ohos.window';
 
 export default class EntryAbility extends UIAbility {
   onCreate(want, launchParam) {
+    // 应用初始化
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
   }
 
   onDestroy() {
+    // onDestroy()在UIAbility实例销毁时触发
+    // 可以用于资源的释放，数据的保存等
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
   onWindowStageCreate(windowStage: window.WindowStage) {
+    // 设置UI页面加载
+    // 设置WindowState的事件订阅（获焦/失焦、可见/不可见）
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
 
-    windowStage.loadContent('pages/Index', (err, data) => {
+    // 使用 windowStage.loadContent设置要加载的页面，并根据需要订阅WindowState的事件
+    // windowStage.loadContent('pages/Index', (err, data) => {
+    windowStage.loadContent('pages/StudentListPage', (err, data) => {
       if (err.code) {
         hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
         return;
@@ -25,16 +32,19 @@ export default class EntryAbility extends UIAbility {
   }
 
   onWindowStageDestroy() {
+    // 释放UI界面资源
     // Main window is destroyed, release UI related resources
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageDestroy');
   }
 
   onForeground() {
+    // 申请系统需要的资源，或者重新申请在onBackground中释放的资源
     // Ability has brought to foreground
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onForeground');
   }
 
   onBackground() {
+    // 释放UI页面不可见时无用的资源，或者在此回调中执行较为耗时的操作（例如状态保存等）
     // Ability has back to background
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onBackground');
   }
